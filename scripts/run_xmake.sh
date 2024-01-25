ecdc8b58117d2061d35a6568fe7605f7d8fc673a
linuxing3 <linuxing3@qq.com>
2024-01-25 11:19:32 +0800

    [fix] cd $1 problem

--- scripts/run_xmake.sh
+++ scripts/run_xmake.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-commands=`echo "install build run project clean config create test" | tr ' ' '\n'`
-selected=`printf "$commands" | fzf`
+commands=$(echo "install build run project clean config create test" | tr ' ' '\n')
+selected=$(printf "$commands" | fzf)
 
-# 
+#
 #     b, build        Build targets if no given tasks.
 #     c, clean        Remove all binary and temporary files.
 #     f, config       Configure the project.
@@ -17,60 +17,57 @@ selected=`printf "$commands" | fzf`
 #        test         Run the project tests.
 #     u, uninstall    Uninstall the project binary files.
 #        update       Update and uninstall the xmake program.
-# 
+#
 # Plugins:
 #        check        Check the project sourcecode and configuration.
 #        doxygen      Generate the doxygen document.
 #        format       Format the current project.
 
 function read_query() {
-    local query_message=$1
+	local query_message=$1
 
-    read -p "$query_message: " query
+	read -p -r "$query_message: " query
 
-    if [[ -z $query ]]; then
-        exit
-    fi
+	if [[ -z $query ]]; then
+		exit
+	fi
 
-    return query
+	return query
 }
-
-# Go to crate root
-cd $1
 
 # Exit if nothing selected
 if [[ -z $selected ]]; then
-    exit
+	exit
 
 # configure project
 elif [ $selected = "project" ]; then
-    query=$(read_query "Project")
-    xmake $selected $query
+	query=$(read_query "Project")
+	xmake $selected $query
 
 # install package
 elif [ $selected = "install" ]; then
-    query=$(read_query "Install")
-    xmake $selected $query
+	query=$(read_query "Install")
+	xmake $selected $query
 
 # change config
 elif [ $selected = "config" ]; then
-    query=$(read_query "Config")
-    xmake $selected $query
+	query=$(read_query "Config")
+	xmake $selected $query
 
 # create new project
 elif [ $selected = "create" ]; then
-    query=$(read_query "Create")
-    xmake $selected -y -l c++ -t tbox.console $query
+	query=$(read_query "Create")
+	xmake $selected -y -l c++ -t tbox.console $query
 
 # Just run selected command
 else
-    xmake $selected
+	xmake $selected
 fi
 
 echo
 echo "COMPLETE"
 
-tmux copy-mode 
+tmux copy-mode
 tmux send-keys 'k0'
 
 # Prevent window to close
